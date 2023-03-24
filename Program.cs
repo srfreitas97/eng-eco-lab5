a652de6b2176c8fcad9dbf25e102a5c7fe7ebad7
Samuel Freitas <srfreitas97@gmail.com>
2023-03-24 09:36:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//Saudação innicial
+Console.WriteLine("Meu nome é Samuel Freitas! Bem vindo ao meu projeto!");
+
+//Objetivo do programa
+Console.WriteLine("O objetivo deste programa é realizar um test drive na ferramenta CK.");
